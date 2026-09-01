50bb9d4afbdef5304914417112e2f12b0372f3b4
Carlos Henrique <henriquesbral@gmail.com>
2026-09-01 13:37:35 -0300

    Subindo nova branch de alteracoes, e adicionando a connectio string de usuarios

---
+++ GestaoFinancas.Application/Services/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoFinancas.Application.Services
+{
+    public class AuthService
+    {
+        private readonly AppContext _context;
+        public AuthService(AppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> IsAuthenticated(string usuario, string senha)
+        {
+            bool autenticado = false;
+
+            autenticado = await AutenticarUsuario();
+
+            return autenticado;
+        }
+
+        private async Task<bool> AutenticarUsuario()
+        {
+            return false;
+        } 
+    }
+}
